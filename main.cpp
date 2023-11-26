8103f947734037d3c68974bded3c378f09eb95c3
CrushedCookieNut <crushedcookienuts@gmail.com>
2023-11-26 09:39:58 +0100

    finished project

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+using namespace std;
+
+const int width = 800, height = 600;
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (NULL==window) {
+        cout << "Could not create window: " << SDL_GetError() << endl;
+        return 1;
+    }
+    SDL_Event windowEvent;
+
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (SDL_QUIT==windowEvent.type) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
